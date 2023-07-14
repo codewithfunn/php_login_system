d9a03999131ab739456efe183646eb17d27db695
jaspreet09915 <jaspreetsingh09915@gmail.com>
2023-07-15 00:56:15 +0530

    first commit

---
+++ partials/_dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+// Connect to Database
+$server ="localhost";
+$username = "root";
+$password = "";
+$database = "users";
+
+$conn = mysqli_connect($server,  $username, $password, $database);
+
+if($conn){
+    // echo "success";
+}
+else{
+    die("Error".mysqli_connect_error());
+}
+
+?>
